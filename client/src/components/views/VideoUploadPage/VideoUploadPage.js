f49bc9e735550ccb9413c7369e407d06682a5710
kureharyosuke <ohhayo.com@gmail.com>
2020-10-18 11:50:27 +0900

    #2. Video Upload Page & Route(true)

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VideoUploadPage() {
+    return (
+        <div className="">
+            VideoUploadPage
+        </div>
+    )
+}
+
+export default VideoUploadPage
